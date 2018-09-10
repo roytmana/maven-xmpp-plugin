fc6ee784f62cedaceedf6770f73140961262cd5f
Alex Roytman <roytmana@alterasoft.net>
2018-09-10 15:57:05 -0400

    Added smtp notification

--- src/main/java/net/alterasoft/maven/xmpp/XmppSendMojo.java
+++ src/main/java/net/alterasoft/maven/xmpp/XmppSendMojo.java
@@ -5,6 +5,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.chat2.ChatManager;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
@@ -12,23 +13,26 @@ import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jxmpp.jid.EntityBareJid;
 import org.jxmpp.jid.impl.JidCreate;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSession;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 @Mojo(name = "send", defaultPhase = LifecyclePhase.DEPLOY)
 public class XmppSendMojo extends AbstractMojo {
+  @Parameter(defaultValue = "${project}", readonly = true)
+  private MavenProject project;
+
   @Parameter(property = "configFile")
   private File configFile;
 
-  @Parameter(property = "configText")
+  @Parameter(property = "configText", defaultValue = "${xmpp-plugin.configText}")
   private String configText;
 
   @Parameter(property = "failOnError", defaultValue = "false", required = true)
@@ -54,6 +58,15 @@ public class XmppSendMojo extends AbstractMojo {
 
   @Parameter(property = "message", defaultValue = "${project.groupId}::${project.artifactId}-${project.version} ${project.build.finalName} has been deployed")
   private String message;
+
+  @Parameter(property = "mailConfig")
+  private Properties mailConfig = new Properties();
+
+  @Parameter(property = "mailConfigText", defaultValue = "${xmpp-plugin.mailConfigText}")
+  private String mailConfigText;
+
+  @Parameter(property = "mailEnabled")
+  private Boolean mailEnabled;
 
   @Override public void execute() throws MojoExecutionException {
     Properties properties = new Properties();
@@ -102,6 +115,18 @@ public class XmppSendMojo extends AbstractMojo {
         recipients = Arrays.asList(p.split("[; ,]"));
       }
     }
+    mailConfigText = mailConfigText == null ? null : mailConfigText.trim();
+    if (mailConfigText != null && !mailConfigText.isEmpty()) {
+      try {
+        properties = new Properties();
+        properties.load(new StringReader(mailConfigText));
+        properties.putAll(this.mailConfig);
+        this.mailConfig = properties;
+      } catch (IOException e) {
+        throw new MojoExecutionException("Error parsing mailConfigText property. It must be a valid java properties format", e);
+      }
+    }
+
     XMPPTCPConnectionConfiguration xmppConfig = null;
     try {
       xmppConfig = XMPPTCPConnectionConfiguration.builder()
@@ -144,6 +169,66 @@ public class XmppSendMojo extends AbstractMojo {
       }
     } finally {
       conn.disconnect();
+    }
+
+    if (Boolean.TRUE.equals(mailEnabled) || mailEnabled == null && mailConfig.getProperty("mail.smtp.host") != null) {
+      final String mailPassword = trimNull(mailConfig.getProperty("password"), password);
+      final Session smtp;
+      if (mailPassword != null) {
+        smtp = Session.getInstance(mailConfig, new Authenticator() {
+          @Override protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(mailConfig.getProperty("mail.smtp.user"), mailPassword);
+          }
+        });
+      } else {
+        smtp = Session.getInstance(mailConfig);
+      }
+//      smtp.setDebug(true);
+      final String rcp = trimNull(mailConfig.getProperty("recipients"));
+      if (rcp != null) {
+        final List<String> emails = Arrays.asList(rcp.split("[; ,]"));
+        MimeMessage msg = new MimeMessage(smtp);
+        try {
+          List<Address> addresses = new ArrayList<>();
+          for (String email: emails) {
+            addresses.add(new InternetAddress(email, true));
+          }
+          msg.setRecipients(Message.RecipientType.TO, addresses.toArray(new Address[0]));
+          msg.setSubject(message);
+          msg.setSentDate(new Date());
+          msg.setText(message, "utf-8", "plain");
+        } catch (MessagingException e) {
+          throw new MojoExecutionException("invalid email configuration", e);
+        }
+        try {
+          getLog().info("Sending " + Arrays.asList(msg.getAllRecipients()));
+          Transport.send(msg);
+          getLog().info("Email has been sent");
+        } catch (Throwable e) {
+          getLog().error("Error sending SMTP message", e);
+          if (failOnError) {
+            throw new MojoExecutionException("Error sending SMTP message", e);
+          }
+        }
+      }
+    }
+  }
+
+  private String trimNull(String s) {
+    if (s == null) {
+      return s;
+    } else {
+      s = s.trim();
+      return s.isEmpty() ? null : s;
+    }
+  }
+
+  private String trimNull(String s, String defaultValue) {
+    if (s == null) {
+      return defaultValue;
+    } else {
+      s = s.trim();
+      return s.isEmpty() ? defaultValue : s;
     }
   }
 
